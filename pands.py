f62525a2ecfac784bad3d30b9353707023e27f51
smmdhaqu <shraju.db@gmail.com>
2026-08-29 09:29:40 +0200

    Practicing Pandass

--- pands.py
+++ pands.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-my_data = [50, 60, 70]
-
-
-convert_data = pd.Series(my_data, index = ["a", "b", "c"])
-convert_data.loc["c"] = 100
-
-print(convert_data)
-print(convert_data[convert_data>=60])
+calories = {"Day 1": 1850, "Day 2": 1900, "Day 3": 2250}
+series_calories = pd.Series(calories)
+print(series_calories [series_calories >= 1900 ])
