f8890f6152e04054ddb533d611eb809ec7e8dad6
smmdhaqu <shraju.db@gmail.com>
2026-08-29 09:39:51 +0200

    Practicing Pandass

--- pands.py
+++ pands.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-calories = {"Day 1": 1850, "Day 2": 1900, "Day 3": 2250}
-series_calories = pd.Series(calories)
-print(series_calories [series_calories >= 1900 ])
+# calories = {"Day 1": 1850, "Day 2": 1900, "Day 3": 2250}
+# series_calories = pd.Series(calories)
+# print(series_calories [series_calories >= 1900 ])
+
+names = ["Bulbasaur", "Ivysaur", "Vanusaur", "Charmander", "Charmeleon", "Charizard"]
+name_convert = pd.Series(names, index = [1, 2, 3, 4, 5, 6])
+
+print(name_convert)
