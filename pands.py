cbbe7cb95ffba3ec73a60fb5accc0007153f4f03
smmdhaqu <shraju.db@gmail.com>
2026-08-31 11:51:02 +0200

    Pandass is great

--- pands.py
+++ pands.py
@@ -4,7 +4,21 @@ import pandas as pd
 # series_calories = pd.Series(calories)
 # print(series_calories [series_calories >= 1900 ])
 
-names = ["Bulbasaur", "Ivysaur", "Vanusaur", "Charmander", "Charmeleon", "Charizard"]
-name_convert = pd.Series(names, index = [1, 2, 3, 4, 5, 6])
+# names = ["Bulbasaur", "Ivysaur", "Vanusaur", "Charmander", "Charmeleon", "Charizard"]
+# name_convert = pd.Series(names, index = [1, 2, 3, 4, 5, 6])
 
-print(name_convert)
+# print(name_convert)
+
+employes = {"Name": ["Shams", "Tuhin", "Shakib"],
+            "Age": [29, 31, 32]
+            }
+df = pd.DataFrame(employes, index= ["Employe 1", "Employe 2", "Employe 3"])
+
+df[["Title", "Contract", "Salary"]] = [
+    ["Data Engineer", "Full-Time", 4000],
+    ["Software Engineer", "Full-Time", 5000],
+    ["Technical Engineer", "Part-Time", 3000]
+    ]
+
+
+print(df)
